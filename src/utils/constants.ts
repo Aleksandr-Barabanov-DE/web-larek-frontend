9faf94827b0495fde15fb89f3d075050f966ab61
Aleksandr Barabanov <alex.de.barabanov@yandex.ru>
2024-06-05 11:25:29 +0200

    Исправил цвета кнопок

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,8 +4,8 @@ export const CDN_URL = `https://larek-api.nomoreparties.co/content/weblarek`;
 export const settings = {};
 
 export const categoryClasses: { [key: string]: string } = {
-	'софт-скил': 'card__category_soft',
-	'хард-скил': 'card__category_hard',
+	'софт-скил':'card__category_softSkill',
+	'хард-скил': 'card__category_hardSkill',
 	кнопка: 'card__category_button',
 	дополнительное: 'card__category_additional',
 	другое: 'card__category_other',
